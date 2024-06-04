7a8eca3def4c4df288adbe3e291255882d5b17ef
blahaj04 <juliopb2004@gmail.com>
2024-06-05 00:14:44 +0200

    commit login google

--- CashCarp/app/src/main/java/com/proyecto/cashcarp/CreateAccountActivity.java
+++ CashCarp/app/src/main/java/com/proyecto/cashcarp/CreateAccountActivity.java
@@ -10,5 +10,6 @@ public class CreateAccountActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account_screen);
     }
 }
