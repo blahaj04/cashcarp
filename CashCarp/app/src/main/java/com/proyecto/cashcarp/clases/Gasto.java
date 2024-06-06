259876c3b92fb8a08a550e0b54bf22b250ea50ea
blahaj04 <juliopb2004@gmail.com>
2024-06-06 22:59:08 +0200

    N̶̨͇̫̫̹͓͇͔͈̄̾̅́̈́͌̚͠͝Ö̴̥̫͌̐͌̒͆̈́̀͌̉͐͠ ̵̧̭͇̰̜͙̯͗̌̿̑͊̊̿͗̚͝ͅḾ̷̗͎͈̱̙̅͆̓̑̚͜ͅĄ̸͖͓͚͇̭̯̭̦͇͔̬̰̆̂͌͆͝ͅȘ̶̡̤̣̩͚̰͐̀̀̏̋̍ ̸͕̺͓͖̭̍͒̂̇̉̒̀̔̂͋̇̕T̷̢̤͉̹̭̰̠͙͇̺̝͉̈́̓̊̈́̌͊̿̑̈͜ͅF̴͍̥͉̩̘̼̘̬̩͇̝̩̞̩̘̑͒̆͂͒̒͋͐͂̋G̵̱̼̖̖̝̞̦͔̞̮̱͇̤͒͊͒̄̎̌͐̇̈̄͆͝ ̶̛̬̬͍̳̮͓̬̮̱̤̝͖͉̤̲͑

--- CashCarp/app/src/main/java/com/proyecto/cashcarp/clases/Gasto.java
+++ CashCarp/app/src/main/java/com/proyecto/cashcarp/clases/Gasto.java
@@ -1,17 +1,31 @@
 package com.proyecto.cashcarp.clases;
 
+import com.google.firebase.Timestamp;
+
 public class Gasto {
 
     private String descripcion;
 
     private double cantidad;
 
+
+    private Timestamp ts;
+
     public Gasto() {
     }
 
-    public Gasto(String descripcion, double cantidad) {
+    public Gasto(String descripcion, double cantidad, Timestamp ts) {
         this.descripcion = descripcion;
         this.cantidad = cantidad;
+        this.ts = ts;
+    }
+
+    public Timestamp getTs() {
+        return ts;
+    }
+
+    public void setTs(Timestamp ts) {
+        this.ts = ts;
     }
 
     public String getDescripcion() {
--- CashCarp/app/src/main/java/com/proyecto/cashcarp/clases/Ingreso.java
+++ CashCarp/app/src/main/java/com/proyecto/cashcarp/clases/Ingreso.java
@@ -1,17 +1,22 @@
 package com.proyecto.cashcarp.clases;
 
+import com.google.firebase.Timestamp;
+
 public class Ingreso {
 
     private String descripcion;
 
     private double cantidad;
 
+    private Timestamp ts;
+
     public Ingreso() {
     }
 
-    public Ingreso(String descripcion, double cantidad) {
+    public Ingreso(String descripcion, double cantidad, Timestamp ts) {
         this.descripcion = descripcion;
         this.cantidad = cantidad;
+        this.ts = ts;
     }
 
     public String getDescripcion() {
