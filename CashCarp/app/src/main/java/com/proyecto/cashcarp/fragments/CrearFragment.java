d3dbb1cbc3a1b9949d6452016b16048549a4d631
blahaj04 <juliopb2004@gmail.com>
2024-06-07 06:19:49 +0200

    si

--- CashCarp/app/src/main/java/com/proyecto/cashcarp/fragments/CrearFragment.java
+++ CashCarp/app/src/main/java/com/proyecto/cashcarp/fragments/CrearFragment.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -19,26 +21,36 @@ import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.proyecto.cashcarp.R;
+import com.proyecto.cashcarp.clases.Gasto;
+import com.proyecto.cashcarp.clases.Ingreso;
 import com.proyecto.cashcarp.clases.TipoGasto;
 import com.proyecto.cashcarp.clases.TipoIngreso;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class CrearFragment extends Fragment {
 
+    private EditText editTextDescripcion, editTextCantidad;
     private SwitchCompat switchGastoIngreso;
     private Spinner spinnerTipo;
-    private Button botonCrearTipo;
+    private Button botonCrearTipo, botonGuardar;
 
     private FirebaseFirestore db;
 
     private SharedPreferences sharedPreferences;
     private String userId;
+
+    private boolean isIngreso = false;
 
     private List<TipoGasto> tipoGastos = new ArrayList<>();
     private List<TipoIngreso> tipoIngresos = new ArrayList<>();
@@ -51,9 +63,12 @@ public class CrearFragment extends Fragment {
         sharedPreferences = getContext().getSharedPreferences("com.proyecto.cashcarp", Context.MODE_PRIVATE);
         userId = sharedPreferences.getString("userId", null);
 
+        editTextDescripcion = view.findViewById(R.id.editTextDescripcion);
+        editTextCantidad = view.findViewById(R.id.editTextCantidad);
         switchGastoIngreso = view.findViewById(R.id.switchGastoIngreso);
         spinnerTipo = view.findViewById(R.id.spinnerTipo);
         botonCrearTipo = view.findViewById(R.id.botonCrearTipo);
+        botonGuardar = view.findViewById(R.id.botonGuardarCrear);
 
         db = FirebaseFirestore.getInstance();
 
@@ -62,59 +77,141 @@ public class CrearFragment extends Fragment {
         mostrarTipos(false);
 
         switchGastoIngreso.setOnCheckedChangeListener((buttonView, isChecked) -> {
+
+            isIngreso = isChecked;
             mostrarTipos(isChecked);
         });
 
-        botonCrearTipo.setOnClickListener(v -> {
+        botonCrearTipo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-            Toast.makeText(getContext(), "Botón crear tipo clickeado", Toast.LENGTH_SHORT).show();
+
+            }
         });
+
+        botonGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String descripcion = editTextDescripcion.getText().toString().trim();
+                String cantidad = editTextCantidad.getText().toString().trim();
+                String tipoSeleccionado = spinnerTipo.getSelectedItem().toString();
+
+                if (descripcion.isEmpty() || cantidad.isEmpty()) {
+
+                    Toast.makeText(getContext(), "Por favor completa todos los campos", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                try {
+                    double cantidadValue = Double.parseDouble(cantidad);
+                    if (cantidadValue <= 0) {
+                        Toast.makeText(getContext(), "La cantidad debe ser mayor que cero", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                } catch (NumberFormatException e) {
+
+                    Toast.makeText(getContext(), "Formato de cantidad inválido", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String tipoId = obtenerId(tipoSeleccionado, isIngreso);
+                guardarObjeto(descripcion, cantidad, tipoId);
+            }
+        });
+
+
         return view;
     }
 
+    private String obtenerId(String tipoSeleccionado, boolean isIngreso) {
+        if (isIngreso) {
+            for (TipoIngreso ti : tipoIngresos) {
+                if (ti.getNombreTipoIngreso().equals(tipoSeleccionado)) {
+                    return ti.getId();
+                }
+            }
+        } else {
+            for (TipoGasto tg : tipoGastos
+            ) {
+                if (tg.getNombreTipoGasto().equals(tipoSeleccionado)) {
+                    return tg.getId();
+                }
+            }
+        }
+        return null;
+    }
+
+    private void guardarObjeto(String descripcion, String cantidad, String idTipo) {
+
+        if (isIngreso) {
+            Ingreso i = new Ingreso(descripcion, Double.parseDouble(cantidad), Timestamp.now());
+
+            db.collection("usuario").document(userId).collection("tipoIngreso").document(idTipo).collection("Ingresos").add(i).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    Toast.makeText(getContext(), "Ingreso guardado correctamente", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(), "Error al guardar el ingreso: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }else{
+            Gasto g = new Gasto(descripcion,Double.parseDouble(cantidad),Timestamp.now());
+
+            db.collection("usuario").document(userId).collection("tipoIngreso").document(idTipo).collection("Ingresos").add(g).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    Toast.makeText(getContext(), "Gasto guardado correctamente", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(), "Error al guardar el gasto: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
     private void cargarTipos() {
-        db.collection("usuario").document(userId)
-                .collection("tipoGasto")
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (DocumentSnapshot document : task.getResult()) {
-                            String tipoId = document.getId();
-                            String colorTipo = document.getString("color");
-                            String nombreTipo = document.getString("nombreTipoGasto");
-                            TipoGasto tg = new TipoGasto(colorTipo, nombreTipo);
-                            tg.setId(tipoId);
-                            tipoGastos.add(tg);
-                        }
-                        // Después de cargar los tipos de gastos, mostrarlos por defecto
-                        mostrarTipos(false);
-                    } else {
-                        Toast.makeText(getContext(), "Error al cargar tipos: " + task.getException(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-        db.collection("usuario").document(userId)
-                .collection("tipoIngreso")
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        for (DocumentSnapshot document : task.getResult()) {
-                            String tipoId = document.getId();
-                            String colorTipo = document.getString("color");
-                            String nombreTipo = document.getString("nombreTipoIngreso");
-                            TipoIngreso ti = new TipoIngreso(colorTipo, nombreTipo);
-                            ti.setId(tipoId);
-                            tipoIngresos.add(ti);
-                        }
-                    } else {
-                        Toast.makeText(getContext(), "Error al cargar tipos: " + task.getException(), Toast.LENGTH_SHORT).show();
-                    }
-                });
+        db.collection("usuario").document(userId).collection("tipoGasto").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (DocumentSnapshot document : task.getResult()) {
+                    String tipoId = document.getId();
+                    String colorTipo = document.getString("color");
+                    String nombreTipo = document.getString("nombreTipoGasto");
+                    TipoGasto tg = new TipoGasto(colorTipo, nombreTipo);
+                    tg.setId(tipoId);
+                    tipoGastos.add(tg);
+                }
+                // Después de cargar los tipos de gastos, mostrarlos por defecto
+                mostrarTipos(false);
+            } else {
+                Toast.makeText(getContext(), "Error al cargar tipos: " + task.getException(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        db.collection("usuario").document(userId).collection("tipoIngreso").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (DocumentSnapshot document : task.getResult()) {
+                    String tipoId = document.getId();
+                    String colorTipo = document.getString("color");
+                    String nombreTipo = document.getString("nombreTipoIngreso");
+                    TipoIngreso ti = new TipoIngreso(colorTipo, nombreTipo);
+                    ti.setId(tipoId);
+                    tipoIngresos.add(ti);
+                }
+            } else {
+                Toast.makeText(getContext(), "Error al cargar tipos: " + task.getException(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void mostrarTipos(boolean isIngreso) {
         List<String> listaNombres = obtenerNombres(isIngreso); // Obtener la lista de tipos según sea un ingreso o un gasto
 
-        Log.d("INFO", "Lista de nombres: " + listaNombres.toString());
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, listaNombres);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -144,4 +241,14 @@ public class CrearFragment extends Fragment {
         return listaNombres;
     }
 
+    private void cambiarColorHeader(View view) {
+        int[] pastelColors = {R.color.pastel_purple, R.color.pastel_yellow, R.color.pastel_pink, R.color.pastel_violet, R.color.pastel_teal, R.color.pastel_peach, R.color.pastel_lavender, R.color.pastel_mint, R.color.pastel_lilac, R.color.pastel_coral, R.color.pastel_brown, R.color.pastel_grey, R.color.pastel_turquoise, R.color.pastel_magenta, R.color.pastel_cyan, R.color.pastel_banana};
+
+        Random random = new Random();
+        int selectedColor = pastelColors[random.nextInt(pastelColors.length)];
+
+
+        LinearLayout linearLayout = view.findViewById(R.id.crear_header_layout);
+        linearLayout.setBackgroundColor(getResources().getColor(selectedColor));
+    }
 }
